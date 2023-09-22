120639a2c4a2945b59a73daf2fc94ee435cdef5c
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2023-09-23 07:41:36 +0900

    📝Modify

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,7 @@ export const config = {
     title: "Kawagoe News",
     teamName: "川越",
     administrator: "ここっぷ",
-    date: "9月22日",
+    date: "9月23日",
     description:
       "川越に関するメディアサイトを集約したサイトです。記事を選択すれば該当記事に遷移します。",
   },
