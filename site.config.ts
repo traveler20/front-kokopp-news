5c1a5483a391052d6b0c1b5c9b75db380ac1912b
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2024-07-07 08:55:04 +0900

    📝Modify

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,7 @@ export const config = {
     title: "川越ニュース",
     teamName: "川越",
     administrator: "ここっぷ",
-    date: "7月6日",
+    date: "7月7日",
     description:
       "川越に関するメディアサイトを集約したサイトです。記事を選択すれば該当記事に遷移します。",
   },
