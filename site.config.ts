b36353e507df4be9e4b2c7b6500f3373d083f3c1
traveler20 <60683849+traveler20@users.noreply.github.com>
2026-02-11 07:31:56 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,7 @@ export const config = {
     title: "川越ニュース",
     teamName: "川越",
     administrator: "kokopp",
-    date: "2月10日",
+    date: "2月11日",
     description:
       "川越に関するメディアサイトを集約したサイトです。記事を選択すれば該当記事に遷移します。",
   },
